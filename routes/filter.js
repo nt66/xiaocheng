d90930e188004865699d7893ac4946272e2e722b
hualingfeng <tc_hualingfeng@163.com>
2015-04-28 06:52:37 +0800

    Delete filter.js

--- routes/filter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/filter.html', function(req, res) {
-    res.render('filter',{title:'商品分类'});
-});
-
-module.exports = router;
